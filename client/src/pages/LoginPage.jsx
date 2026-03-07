c28310b9819b59d12d243b29c5edcae316518ef9
hedge360 <scion5u@gmail.com>
2026-03-07 15:21:59 -0800

    changed logo

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -10,7 +10,7 @@ export default function LoginPage() {
       <div className="login-card">
         <div className="login-header">
           <div className="login-logo-wrap">
-            <img src="/Thoth.svg" alt="Thoth" className="login-logo-img" />
+            <img src="/Thoth.png" alt="Thoth" className="login-logo-img" />
           </div>
           <h1>Thoth</h1>
           <p>Sync your Google Classroom assignments</p>
